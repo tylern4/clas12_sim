e06762982b9e0e4847f323259ec43fb90b96ad05
Nicholas Tyler <tylern@canisius.edu>
2018-05-23 10:36:42 -0400

    Do gen before job

--- submit/sim.sh
+++ submit/sim.sh
@@ -4,17 +4,17 @@ source /site/12gev_phys/softenv.sh 2.2 2> /dev/null
 export TORUS_FEILD=-1.0
 export SOLO_FEILD=1.0
 export RUN_NUM=3050
-export NUM_GEN=1000
+export NUM_GEN=1000000
 
 export CLARA_HOME=/home/tylern/clara/5c.3.5
 export COATJAVA=$CLARA_HOME/plugins/clas12
 export PATH=$PATH:$CLARA_HOME/bin:$COATJAVA/bin
 
-echo "RUNNING SIMPLE GENERATOR"
-javac -cp "$COATJAVA/clas12/lib/clas/*" RandomEventGenerator.java
-java -cp "$COATJAVA/clas12/lib/clas/*:." RandomEventGenerator $NUM_GEN
-echo "RAN GENERATOR"
-ls -la
+#echo "RUNNING SIMPLE GENERATOR"
+#javac -cp "$COATJAVA/clas12/lib/clas/*" RandomEventGenerator.java
+#java -cp "$COATJAVA/clas12/lib/clas/*:." RandomEventGenerator $NUM_GEN
+#echo "RAN GENERATOR"
+#ls -la
 
 
 echo "RUNNING GEMC"
